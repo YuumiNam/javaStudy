1d119ae1391432124d3e9884d2d7a6997626253b
dltjsam <dltjsam@gmail.com>
2022-10-28 14:34:03 +0900

    chapter03

--- chapter03/src/main/java/chapter03/ObjectTest01.java
+++ chapter03/src/main/java/chapter03/ObjectTest01.java
@@ -5,8 +5,11 @@ public class ObjectTest01 {
 		Point p = new Point(10, 20);
 		
 		// 암시적으로 Object 클래스의 상속을 받는다는 것을 보여줌
-		System.out.println(p.getClass());
-		System.out.println(p.hashCode());
-		System.out.println(p.toString());
+		// Class klass = p.getClass();  upcasting의 개념이 아님
+		System.out.println(p.getClass()); // reflection  System.out.println(klass);
+		System.out.println(p.hashCode()); // address기반의 해싱값 (해싱 : int가 아닌 값들을 int값으로 넣어서 비교)
+										  // address X, reference X
+		System.out.println(p.toString()); // getClass() + "@" + hashCode()
+		System.out.println(p);
 	}
 }
--- chapter03/src/main/java/chapter03/Point.java
+++ chapter03/src/main/java/chapter03/Point.java
@@ -5,9 +5,17 @@ public class Point {
 	private int x;
 	private int y;
 	
+	
 	// 생성자
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+
+	
+	// 메소드
+//	@Override
+//	public String toString() {
+//		return "Point [x=" + x + ", y=" + y +"]";
+//	}
 }
