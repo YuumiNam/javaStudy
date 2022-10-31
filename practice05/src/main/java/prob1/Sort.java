903c891ec7e62716b97f290636482095b020845c
dltjsam <dltjsam@gmail.com>
2022-10-31 09:40:51 +0900

    practice

--- practice05/src/main/java/prob1/Sort.java
+++ practice05/src/main/java/prob1/Sort.java
@@ -4,13 +4,13 @@ public class Sort {
 	
 	public static void main(String[] arg) {
 	
-		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
+		int array[] = {5, 9, 3, 8, 60, 20, 1};
 		int count =  array.length;
 		
-		System.out.println( "Before sort." );
+		System.out.println("Before sort.");
 		
 		for (int i = 0; i < count; i++) {
-			System.out.print( array[ i ] + " " );
+			System.out.print(array[i] + " ");
 		}
 		
 		//
@@ -19,7 +19,7 @@ public class Sort {
 
 		
 		// 결과 출력
-		System.out.println( "\nAfter Sort." );
+		System.out.println("\nAfter Sort.");
 		
 		for (int i = 0; i < count; i++) {
 			System.out.print(array[i] + " ");
