d81019e7e723598ada685f93aa2aef9aeaf0f081
dltjsam <dltjsam@gmail.com>
2022-10-31 20:35:59 +0900

    chapter01

--- chapter01/src/main/java/chapter01/Ex26Continue.java
+++ chapter01/src/main/java/chapter01/Ex26Continue.java
@@ -1,7 +1,7 @@
 package chapter01;
 
 public class Ex26Continue {
-	//1부터 20까지의 정수중 3의배수를 빼고 출력하라
+	//1부터 20까지의 정수중 2의배수를 빼고 출력하라
 	public static void main(String[] args) {
 		for(int i = 0 ; i < 20 ; i++) {
 			if (i % 2 == 0) {
