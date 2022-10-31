155f20ef07d517bbd4c3e1a7ab26375877b0ffc1
dltjsam <dltjsam@gmail.com>
2022-10-31 10:19:33 +0900

    chapter03

--- chapter03/src/main/java/chapter03/ObjectTest02.java
+++ chapter03/src/main/java/chapter03/ObjectTest02.java
@@ -11,10 +11,9 @@ public class ObjectTest02 {
 		System.out.println(p2 == p3);
 		
 		// equals : 두 객체의 동질성 (내용비교)
-		// 부모클래스 Object의 기본 구현은 동일성(==) 비교와 같다.
 		System.out.println(p1.equals(p2)); // 하지만 결과적으로는 동일성비교가 되어버림
-										   // object의 equals()메소드를 만든사람은 지금 우리가 뭔 객체를 비교할지를 모르기때문에 기본 리턴값을 동일성(==)비교로 설정
-										   // 그러므로 여기서 내용비교를 하려면 Point 클래스에 equals()메소드를 override를 한다음 써야함
+										   // object의 equals()메소드를 만든사람은 지금 우리가 뭔 객체를 비교할지를 모르기때문에 기본 리턴값을 동일성(==)비교로 설정했다.
+										   // 그러므로 여기서 내용비교 equals()를 쓰려면 Point 클래스에 equals()메소드를 override를 한다음 써야함
 		System.out.println(p2.equals(p3));
 	}
 }
