de09ca1475a7ec9c0d98ba7be385c15a90779ebf
dltjsam <dltjsam@gmail.com>
2022-10-31 12:01:07 +0900

    chapter03

--- chapter03/src/main/java/chapter03/ObjectTest02.java
+++ chapter03/src/main/java/chapter03/ObjectTest02.java
@@ -14,8 +14,6 @@ public class ObjectTest02 {
 		System.out.println(p1.equals(p2)); // 하지만 결과적으로는 동일성비교가 되어버림
 										   // object의 equals()메소드를 만든사람은 지금 우리가 뭔 객체를 비교할지를 모르기때문에 기본 리턴값을 동일성(==)비교로 설정했다.
 										   // 그러므로 여기서 내용비교 equals()를 쓰려면 Point 클래스에 equals()메소드를 override를 한다음 써야함
-										   // 이때 hashCode()도 같이 override를 하게되는데,
-										   // 해시코드값이 다르면 다른 객체로 판단하고, 해시 코드값이 같으면 그때 equals()메소드로 다시 비교해서 레퍼런스 값도 같다면 동등 객체로 판단함
 		System.out.println(p2.equals(p3));
 	}
 }
