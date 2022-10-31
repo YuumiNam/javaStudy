c0b9db5b15f9017a49b200aa84d1200cdbf8e304
dltjsam <dltjsam@gmail.com>
2022-10-31 09:22:59 +0900

    practice

--- practice05/src/main/java/prob2/MusicPhone.java
+++ practice05/src/main/java/prob2/MusicPhone.java
@@ -3,7 +3,7 @@ package prob2;
 public class MusicPhone extends Phone {
 	@Override
 	public void execute (String function) {
-	      if (function.equals("음악")) {
+	      if(function.equals("음악")) {
 	          playMusic();
 	          return;
 	      }
